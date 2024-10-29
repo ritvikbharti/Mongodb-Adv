1a152069f9522923a20be1567133ea8b8bfd2721
Ritvik Bharti <bhartiritvik1000@gmail.com>
2024-10-30 02:10:30 +0530

    commit

--- app.js
+++ app.js
@@ -9,11 +9,23 @@ app.get('/',(req,res)=>{
 
 app.get('/create', async (req,res)=>{
   let createuser =  await userModel.create({
-        name:"Ritvik",
-        email:"bhartiritvik1000@gmail.com",
-        username:"ritvik_bharti_01"
+        name:"Tamanna",
+        email:"tamanna1000@gmail.com",
+        username:"tamanna_malik_01"
     })
     res.send(createuser)
+})
+app.get('/update', async (req,res)=>{
+   let updateduser = await userModel.findOneAndUpdate({
+    username:"ritvik_bharti_01"},
+    {name:"Adesh"},
+    {new : true})
+    res.send(updateduser)
+})
+
+app.get('/read',async (req,res)=>{
+    let users = await userModel.find();
+    res.send(users)
 })
 
 
